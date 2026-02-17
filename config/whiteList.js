2a8da8c65314bf13bb0032c3e694d63084b38553
masangakevin256 <masangakevin1@gmail.com>
2026-02-17 11:35:47 +0300

    adding filds to user input

--- config/whiteList.js
+++ config/whiteList.js
@@ -1,7 +1,8 @@
 //origins that are allowed to access the server
 const allowedOrigins = [
     "http://localhost:3000",
-    "http://localhost:3001"
+    "http://localhost:3001",
+    "http://localhost:8080"
 ];
 
 export default allowedOrigins;
